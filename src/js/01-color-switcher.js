6064fca36f0416ff3c0e24505f21b0909c5d986e
TalyaMonya <128909188+TalyaMonya@users.noreply.github.com>
2023-08-20 21:53:37 +0300

    Done Task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const ref = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-start'),
+    btnStop: document.querySelector('button[data-stop]')
+}
 
+const CHANGE_COLOR_DELAY = 1000;
+let idInterval = null;
+
+
+ref.btnStop.disabled = true;
+ref.btnStart.addEventListener('click', handlerStartChangeColor);
+ref.btnStop.addEventListener('click', handlerStopChangeColor)
+
+function handlerStartChangeColor() {
+    ref.btnStart.disabled = true;
+    ref.btnStop.disabled = false;
+
+    idInterval = setInterval(() => {
+        ref.body.style.backgroundColor = getRandomHexColor();
+    }, CHANGE_COLOR_DELAY);
+}
+
+
+
+function handlerStopChangeColor() {
+    ref.btnStart.disabled = false;
+    ref.btnStop.disabled = true;
+
+    clearInterval(idInterval);
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
